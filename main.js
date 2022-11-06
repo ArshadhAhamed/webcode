a01655340e4ebf98738b78c23a0ba2f7660ec16c
ArshadhAhamed <98503213+ArshadhAhamed@users.noreply.github.com>
2022-11-06 00:08:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -44,35 +44,12 @@ function MakeupkitList(){
 }
 
 
-// get recipe of the meal
-function getMealRecipe(e){
-    e.preventDefault();
-    if(e.target.classList.contains('recipe-btn')){
-        let mealItem = e.target.parentElement.parentElement;
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
-        .then(response => response.json())
-        .then(data => mealRecipeModal(data.meals));
-    }
-}
-
-// create a modal
-function mealRecipeModal(meal){
-    console.log(meal);
-    meal = meal[0];
-    let html = `
-        <h2 class = "recipe-title">${meal.strMeal}</h2>
-        <p class = "recipe-category">${meal.strCategory}</p>
-        <div class = "recipe-instruct">
-            <h3>Instructions:</h3>
-            <p>${meal.strInstructions}</p>
-        </div>
-        <div class = "recipe-meal-img">
-            <img src = "${meal.strMealThumb}" alt = "">
-        </div>
-        <div class = "recipe-link">
-            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
-        </div>
-    `;
-    mealDetailsContent.innerHTML = html;
-    mealDetailsContent.parentElement.classList.add('showRecipe');
-}
+function Products()=fetch('https://makeup-api.herokuapp.com/api/v1/products.json')
+.then((response) => {
+  console.log(response);
+  // use chaining by returning the promise
+  return response.json(); // response.json() return a promise
+})
+.then((products) => {
+    console.log(products)
+})
